130076ff0a6c659d144b54b3dfc4103918cdf96f
L Bharath <61648871+bharathgowda1507@users.noreply.github.com>
2020-09-10 08:15:39 +0530

    Create Climbing the Leaderboard.py

---
+++ Climbing the Leaderboard.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the climbingLeaderboard function below.
+def climbingLeaderboard(scores, alice):
+    scores = list(set(scores))
+    scores.sort(reverse = True)
+    ranks = list()
+
+    for i in alice:
+        scores.append(i)
+        scores = list(set(scores))
+        scores.sort(reverse = True)
+        ranks.append(scores.index(i)+1)
+
+    return ranks
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    scores_count = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    alice_count = int(input())
+
+    alice = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(scores, alice)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
